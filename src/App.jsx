630ca8835e166a32d358ab0e87a83c88d3a8c65a
Damini Singh <daminisingh9761@gmail.com>
2026-05-15 17:19:17 +0530

    Initial project setup

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+ function App() {
+  return(
+    <div>
+      <h1>School ERP System</h1>
+      <p>Welcome to the School ERP System. Please select an option from the menu.</p>
+    </div>
+  );
+ }
+ export default App;
